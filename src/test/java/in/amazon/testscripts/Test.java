3a0ce34459c5ac069b10281905c151d0c4c5bc0b
Deepinder <deebhatti@gmail.com>
2021-09-29 22:11:08 -0400

    Added day() method in Test class

---
+++ src/test/java/in/amazon/testscripts/Test.java
@@ -0,0 +1,13 @@
+package in.amazon.testscripts;
+
+public class Test {
+
+    public void day(){
+        System.out.println("Today is Wednesday");
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.day();
+    }
+}
