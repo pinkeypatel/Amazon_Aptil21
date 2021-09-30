890070f85633aacf4164cb537aff016000eb7f17
Deepinder <deebhatti@gmail.com>
2021-09-29 22:26:48 -0400

    Added month() method

--- src/test/java/in/amazon/testscripts/Test.java
+++ src/test/java/in/amazon/testscripts/Test.java
@@ -1,13 +1,19 @@
 package in.amazon.testscripts;
 
 public class Test {
+    String a = "Hello";
 
     public void day(){
         System.out.println("Today is Wednesday");
     }
 
+    public void month(){
+        System.out.println("It is September");
+    }
+
     public static void main(String[] args) {
         Test test = new Test();
         test.day();
+        test.month();
     }
 }
