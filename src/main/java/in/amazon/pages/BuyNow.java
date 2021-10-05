3bb8278fe0f9ad597d649f4b29f41ddead362f90
suran <suranicharms@gmail.com>
2021-10-04 21:04:01 -0500

    Revert "done"
    This reverts commit 5269a101

--- src/main/java/in/amazon/pages/BuyNow.java
+++ src/main/java/in/amazon/pages/BuyNow.java
@@ -11,16 +11,16 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class BuyNow {
     WebDriverWait wait;
 
-    @FindBy(linkText = "Join Prime")
-    private WebElement joinPrimebtn;
+    @FindBy(id="buy-now-butto")
+    private WebElement buyNowBtn;
 
     public BuyNow(WebDriver driver){
         PageFactory.initElements(driver, this);
         wait = new WebDriverWait(driver,30);
     }
 
-    public void clickjoinPimebtn(){
-        wait.until(ExpectedConditions.visibilityOf(joinPrimebtn));
-        joinPrimebtn.click();
+    public void clickBuyNowBtn(){
+        wait.until(ExpectedConditions.visibilityOf(buyNowBtn));
+        buyNowBtn.click();
     }
 }
