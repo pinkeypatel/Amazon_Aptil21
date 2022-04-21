30f569de14e9dbe5ab23a0982ab83221eaabdd82
pinkeypatel <charu.patel2015@gmail.com>
2022-04-21 11:27:25 -0500

    added Tc_103

--- src/test/java/utils/Driver.java
+++ src/test/java/utils/Driver.java
@@ -13,7 +13,7 @@ public class Driver {
     protected static BuyNow buyNow ;
 
     public static void init(){
-        System.setProperty("webdriver.chrome.driver", "resources//chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "Resource//chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://amazon.in");
